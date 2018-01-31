e2ee77ef5e06bc84ec7304446f55c3f2ff4b79e4
poorniii <35651828+poorniii@users.noreply.github.com>
2018-01-31 16:29:09 +0530

    LARGEST.C
    FIND LARGEST NUMBER AMONG THREE NUMBERS ENTERED BY USER

---
+++ LARGEST.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+double n1,n2,n3;
+printf("enter the three different numbers:");
+scanf("%1f %1f %1f",&n1,&n2,&n3);
+if(n1>=n2 && n1>=n3)
+printf("%2f is the largest number:",n1);
+if(n2>=n1 && n2>=n3)
+printf("%2f is the largest number:",n2);
+if(n3>=n1 && n3>=n2)
+return o;
+}
